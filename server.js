2215134b61e46d020965045350a37cca26c3e7eb
root <root@ip-172-31-37-206.ap-northeast-1.compute.internal>
2022-01-26 16:46:12 +0530

    resolved for testing

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function(req,res){
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello World!');
+        res.end('Hello World! this world is full of data');
 }).listen(8081);
 console.log('Server started on localhost:8081; press Ctrl-C to terminate...!');
